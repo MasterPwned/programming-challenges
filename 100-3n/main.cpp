f3ea44ab74eb27b5c38184775cddc823f4fded82
Grant Walter <grant.w@aol.com>
2018-09-18 21:49:39 -0500

    attempt 1 at challenge problem

--- 100-3n/main.cpp
+++ 100-3n/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int calculate(value) {
+	int counter = 1;
+	while (value != 1) {
+		if (value % 2 = 0) {
+			value /= 2;
+			counter += 1;
+		} else {
+			value = (value * 3) + 1;
+			counter += 1;
+		}
+	}
+	return counter;
+}
 
+int main() {
+	int i, j, cycleLength;
+	int maxCycleLength = 0;
+	cout <<"Lower and upper bound: ";
+	cin >>i >>j;
+	
+	for (int x = i; x <= j; x++) {
+		cycleLength = calculate(x);
+		if (cycleLength > maxCycleLength) {
+			maxCycleLength = cycleLength;
+		}	
+	}
+	
+	cout <<i <<" " <<j <<" " <<; //TODO: put in varible
+
+	return 0;
 }
