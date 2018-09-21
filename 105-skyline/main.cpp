05f404580f4434460976d7bc09a4d4fe2fb3ae4f
Grant Walter <grant.w@aol.com>
2018-09-21 14:56:49 -0500

    starting code

---
+++ 105-skyline/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int height[10001] { };
+	int left, high, right,
+	while (cin >> left) {
+		cin >> high >> right;
+		
+	}
+	
+	return 0;
+}
